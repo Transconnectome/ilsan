5bcbcbaff1af26f0eb1ff239e40366a797927600
HEEHWANWANG <67574748+HEEHWANWANG@users.noreply.github.com>
2024-08-20 15:42:21 +0900

    Update main_Bblip_v1.py

--- project/main_Bblip_v1.py
+++ project/main_Bblip_v1.py
@@ -73,7 +73,7 @@ def __main__():
     ### checkpoint 
     checkpoint_callback = ModelCheckpoint(
         save_top_k=2,
-        monitor="valid/loss",
+        monitor="valid/loss_total",
         mode="min",
         dirpath=config.pl_trainer.ckpt_dir,
         filename="{epoch:02d}-{valid_loss_total:.2f}",
